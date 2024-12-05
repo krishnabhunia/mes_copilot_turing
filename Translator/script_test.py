104a30e18a465e5d486e713cb53716f1afc8c9cd
Krishna Bhunia <krishna-d.bhunia@capgemini.com>
2024-12-05 13:02:36 +0530

    working code with offline mode and removinf tensorflow warning

--- Translator/script_test.py
+++ Translator/script_test.py
@@ -13,26 +13,26 @@ os.environ["HF_HOME"] = "cache"
 
 # Delete the folder if it exists
 if os.path.exists(output_folder):
-    print('folder existed, deleted')
+    print('folder existed, so deleting ...')
     shutil.rmtree(output_folder)
-    print('folder deleted', output_folder)
+    print('folder deleted : ', output_folder)
 
 # Recreate the folder
 os.makedirs(output_folder, exist_ok=True)
 
 # Load MarianMT model and tokenizer
 model_name = 'Helsinki-NLP/opus-mt-en-fr'
-print("tokenizer intializing")
+print("tokenizer intializing ...")
 tokenizer = MarianTokenizer.from_pretrained(model_name)
-print("tokenizer initialized, model initializing")
+print("tokenizer initialized, model initializing ...")
 model = MarianMTModel.from_pretrained(model_name)
 print("model initialized")
 
 # Translate function
 def translate_text(text, tokenizer, model):
-    print('token encoding')
+    print('token encoding ...')
     tokens = tokenizer.encode(text, return_tensors='pt', max_length=512, truncation=True)
-    print('token encoded, model generating tokens')
+    print('token encoded, model generating tokens ...')
     translated_tokens = model.generate(tokens, max_length=512, num_beams=5, early_stopping=True)
     print('translated token generated, returning tokens')
     return tokenizer.decode(translated_tokens[0], skip_special_tokens=True)
