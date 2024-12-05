eb92ca60324db56f70b67d3286d552523ea608cd
Krishna Bhunia <krishna-d.bhunia@capgemini.com>
2024-12-05 12:02:43 +0530

    ignore

--- Translator/file_read.py
+++ Translator/file_read.py
@@ -1,10 +1,17 @@
 import os
 from transformers import MarianMTModel, MarianTokenizer # type: ignore
 
+
 # Define paths
 input_folder = 'input'
 output_folder = 'translated_document'
-custom_cache_directory = '~/.cache/huggingface/transformers'
+custom_cache_directory = 'cache'
+os.environ["HF_HOME"] = "cache"
+print(os.environ.get("HF_HOME"))
+
+# Suppress TensorFlow logs
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Suppress TensorFlow logs
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # Disable oneDNN optimizations
 
 # Create output folder if not exists
 os.makedirs(output_folder, exist_ok=True)
@@ -19,13 +26,11 @@ model_name = 'Helsinki-NLP/opus-mt-en-fr'
 tokenizer = MarianTokenizer.from_pretrained(model_name, cache_dir=custom_cache_directory)
 model = MarianMTModel.from_pretrained(model_name, cache_dir=custom_cache_directory)
 
-
 # Function to translate text
 def translate_text(text, tokenizer, model):
     tokens = tokenizer.encode(text, return_tensors='pt', max_length=512, truncation=True)
     translated_tokens = model.generate(tokens, max_length=512, num_beams=5, early_stopping=True)
     return tokenizer.decode(translated_tokens[0], skip_special_tokens=True)
-
 
 input_file_name = 'krishna.txt'
 input_file_path = f'{input_folder}/{input_file_name}'
@@ -41,7 +46,6 @@ if os.path.isfile(input_file_path):
     translated_content = translate_text(content, tokenizer, model)
     print('Translate complete')
     print(translated_content)
-
 
     output_file_name = f'translated_{input_file_name}'
     output_file_path = f'{output_folder}/{output_file_name}'
