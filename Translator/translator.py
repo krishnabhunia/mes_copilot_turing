ef2548fcbf0e04d483aa55fdac4dd7ad7780866a
Krishna Bhunia <krishna-d.bhunia@capgemini.com>
2024-12-12 18:20:34 +0530

    try except block

--- Translator/translator.py
+++ Translator/translator.py
@@ -62,180 +62,210 @@ class Translator:
 
 
     def initialize_translator(self):
-        base_name = os.getenv("TRANSFORMER_BASE_MODEL_NAME") or "Helsinki-NLP"
-        translation_type = os.getenv("TRANSLATION_TYPE") or "opus-mt"
-        self.tensor_type = Translator.get_tensor(os.getenv("TENSOR_TYPE")) or Translator.get_tensor("pytorch")
-        self.model_name = f"{base_name}/{translation_type}-{self.source_lang}-{self.target_lang}"
-        print(f"Translator Name : {self.model_name}")
-        self.tokenizer = MarianTokenizer.from_pretrained(self.model_name)
-        self.model = MarianMTModel.from_pretrained(self.model_name)
+        try:
+            base_name = os.getenv("TRANSFORMER_BASE_MODEL_NAME") or "Helsinki-NLP"
+            translation_type = os.getenv("TRANSLATION_TYPE") or "opus-mt"
+            self.tensor_type = Translator.get_tensor(os.getenv("TENSOR_TYPE")) or Translator.get_tensor("pytorch")
+            self.model_name = f"{base_name}/{translation_type}-{self.source_lang}-{self.target_lang}"
+            print(f"Translator Name : {self.model_name}")
+            self.tokenizer = MarianTokenizer.from_pretrained(self.model_name)
+            self.model = MarianMTModel.from_pretrained(self.model_name)
+        except Exception as ex:
+            print(ex)
 
     @staticmethod
     def get_tensor(req_tensor):
-        if req_tensor.lower() == "pytorch":
-            return 'pt'
+        try:
+            if req_tensor.lower() == "pytorch":
+                return 'pt'
 
-        if req_tensor.lower() == "tensorflow":
-            return 'tf'
+            if req_tensor.lower() == "tensorflow":
+                return 'tf'
 
-        if req_tensor.lower() == "numpy":
-            return 'np'
+            if req_tensor.lower() == "numpy":
+                return 'np'
 
-        if req_tensor.lower() == "jax":
-            return 'jax'
+            if req_tensor.lower() == "jax":
+                return 'jax'
 
-        if req_tensor.lower() == "mlx":
-            return 'mlx'
+            if req_tensor.lower() == "mlx":
+                return 'mlx'
+        except Exception as ex:
+            print(ex)
 
     def delete_output_folder(self):
-        print(f"Deleting Output Folder : {self.output_folder} ... ")
-        if os.path.exists(self.output_folder):  # type : ignore
-            print("Path exists , deleting folder ...")
-            shutil.rmtree(self.output_folder)
-            print(f"Folder deleted {self.output_folder}")
-        else:
-            print(f"Folder doesn't exists : {self.output_folder}")
-        print(f"Deleting Temporary Folder : {self.temp_folder} ... ")
-        if os.path.exists(self.temp_folder):
-            print("Removing temporary folders : {self.temp_folder} ... ")
-            shutil.rmtree(self.temp_folder)
-            print(f"Temporary folder removed : {self.temp_folder}")
+        try:
+            print(f"Deleting Output Folder : {self.output_folder} ... ")
+            if os.path.exists(self.output_folder):  # type : ignore
+                print("Path exists , deleting folder ...")
+                shutil.rmtree(self.output_folder)
+                print(f"Folder deleted {self.output_folder}")
+            else:
+                print(f"Folder doesn't exists : {self.output_folder}")
+            print(f"Deleting Temporary Folder : {self.temp_folder} ... ")
+            if os.path.exists(self.temp_folder):
+                print("Removing temporary folders : {self.temp_folder} ... ")
+                shutil.rmtree(self.temp_folder)
+                print(f"Temporary folder removed : {self.temp_folder}")
+        except Exception as ex:
+            print(ex)
 
     def translate(self, text):
-        sentences = text.split("\n")
-        translated = []
-        current_chunk = []
-        current_length = 0
+        try:
+            sentences = text.split("\n")
+            translated = []
+            current_chunk = []
+            current_length = 0
 
-        for sentence in sentences:
-            if current_length + len(sentence) <= self.chunk_size:
-                current_chunk.append(sentence)
-                current_length += len(sentence)
-            else:
+            for sentence in sentences:
+                if current_length + len(sentence) <= self.chunk_size:
+                    current_chunk.append(sentence)
+                    current_length += len(sentence)
+                else:
+                    tokens = self.tokenizer.encode(" ".join(current_chunk), return_tensors=self.tensor_type, max_length=self.translate_text_length, truncation=True)
+                    translated_tokens = self.model.generate(tokens, max_length=self.translate_text_length, num_beams=5, early_stopping=True)
+                    translated.append(self.tokenizer.decode(translated_tokens[0], skip_special_tokens=True))
+                    current_chunk = [sentence]
+                    current_length = len(sentence)
+
+            if current_chunk:
                 tokens = self.tokenizer.encode(" ".join(current_chunk), return_tensors=self.tensor_type, max_length=self.translate_text_length, truncation=True)
                 translated_tokens = self.model.generate(tokens, max_length=self.translate_text_length, num_beams=5, early_stopping=True)
                 translated.append(self.tokenizer.decode(translated_tokens[0], skip_special_tokens=True))
-                current_chunk = [sentence]
-                current_length = len(sentence)
 
-        if current_chunk:
-            tokens = self.tokenizer.encode(" ".join(current_chunk), return_tensors=self.tensor_type, max_length=self.translate_text_length, truncation=True)
-            translated_tokens = self.model.generate(tokens, max_length=self.translate_text_length, num_beams=5, early_stopping=True)
-            translated.append(self.tokenizer.decode(translated_tokens[0], skip_special_tokens=True))
-
-        return "\n".join(translated)
+            return "\n".join(translated)
+        except Exception as ex:
+            print(ex)
 
     def get_document_type(self, file_name):
-        endswith = os.path.splitext(file_name)[1]
-        if endswith in [".docs", ".doc", ".docx"]:
-            return "word"
-        elif endswith in [".pdf"]:
-            return "pdf"
-        elif endswith in [".txt", ".text"]:
-            return "txt"
+        try:
+            endswith = os.path.splitext(file_name)[1]
+            if endswith in [".docs", ".doc", ".docx"]:
+                return "word"
+            elif endswith in [".pdf"]:
+                return "pdf"
+            elif endswith in [".txt", ".text"]:
+                return "txt"
+        except Exception as ex:
+            print(ex)
 
     def extract_text_from_pdf(self, pdf_path):
-        reader = PdfReader(pdf_path)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
-        return text
+        try:
+            reader = PdfReader(pdf_path)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
+            return text
+        except Exception as ex:
+            print(ex)
 
     def extract_files_for_translating(self):
-        input_file_path = os.path.join(self.input_folder, self.file_name)
-        # output_file_path = os.path.join(self.output_folder, self.file_name)
-        # Step 1: Extract the .docx contents
-        os.makedirs(self.temp_folder, exist_ok=True)
-        with zipfile.ZipFile(input_file_path, 'r') as docx_zip:
-            docx_zip.extractall(self.temp_folder)
+        try:
+            input_file_path = os.path.join(self.input_folder, self.file_name)
+            # output_file_path = os.path.join(self.output_folder, self.file_name)
+            # Step 1: Extract the .docx contents
+            os.makedirs(self.temp_folder, exist_ok=True)
+            with zipfile.ZipFile(input_file_path, 'r') as docx_zip:
+                docx_zip.extractall(self.temp_folder)
 
-        # Step 2: Extract plain text data from document.xml
-        plain_text_data = []
+            # Step 2: Extract plain text data from document.xml
+            plain_text_data = []
 
-        document_xml_path = os.path.join(self.temp_folder, self.get_document_type(self.file_name), "document.xml")  # type:ignore
-        if os.path.exists(document_xml_path):
-            # Parse the document.xml file to extract text
-            tree = ET.parse(document_xml_path)
-            root = tree.getroot()
-            namespace = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+            document_xml_path = os.path.join(self.temp_folder, self.get_document_type(self.file_name), "document.xml")  # type:ignore
+            if os.path.exists(document_xml_path):
+                # Parse the document.xml file to extract text
+                tree = ET.parse(document_xml_path)
+                root = tree.getroot()
+                namespace = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
 
-            # Find all text nodes in the XML
-            for text_node in root.findall(".//w:t", namespace):
-                plain_text_data.append({text_node.text: ""})  # type : ignore  # Add plain text as key with blank value
+                # Find all text nodes in the XML
+                for text_node in root.findall(".//w:t", namespace):
+                    plain_text_data.append({text_node.text: ""})  # type : ignore  # Add plain text as key with blank value
 
-        # Step 3: Write plain text data to JSON
-        json_file = f"{self.temp_folder}/{self.temp_file}.{self.temp_file_extension}"
-        with open(json_file, 'w', encoding='utf-8') as f:
-            json.dump(plain_text_data, f, indent=4)
-        print(f"JSON file with plain text data created: {json_file}")
+            # Step 3: Write plain text data to JSON
+            json_file = f"{self.temp_folder}/{self.temp_file}.{self.temp_file_extension}"
+            with open(json_file, 'w', encoding='utf-8') as f:
+                json.dump(plain_text_data, f, indent=4)
+            print(f"JSON file with plain text data created: {json_file}")
+        except Exception as ex:
+            print(ex)
 
     def translate_extracted_file(self):
-        self.initialize_translator()
-        # Read the JSON file
-        input_file = f"{self.temp_folder}/{self.temp_file}.{self.temp_file_extension}"
-        with open(input_file, 'r', encoding='utf-8') as file:
-            data = json.load(file)
+        try:
+            self.initialize_translator()
+            # Read the JSON file
+            input_file = f"{self.temp_folder}/{self.temp_file}.{self.temp_file_extension}"
+            with open(input_file, 'r', encoding='utf-8') as file:
+                data = json.load(file)
 
-        # Translate and populate values
-        # count = 0
-        print("Translating started ...")
-        start_time = datetime.now()
-        for da in tqdm(data, desc="Translating : ", unit=" Lines"):
-            for d in da.items():
-                da[d[0]] = self.translate(d[0])
-            # count += 1
-            # print(f"Percentage complete : {(count * 100 / len(data)):.2f} %", end="\r")
-        time_difference = datetime.now() - start_time
-        hours, remainder = divmod(time_difference.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        print(f"Time taken: {hours} hours, {minutes} minutes, {seconds} seconds")
+            # Translate and populate values
+            # count = 0
+            print("Translating started ...")
+            start_time = datetime.now()
+            for da in tqdm(data, desc="Translating : ", unit=" Lines"):
+                for d in da.items():
+                    da[d[0]] = self.translate(d[0])
+                # count += 1
+                # print(f"Percentage complete : {(count * 100 / len(data)):.2f} %", end="\r")
+            time_difference = datetime.now() - start_time
+            hours, remainder = divmod(time_difference.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            print(f"Time taken: {hours} hours, {minutes} minutes, {seconds} seconds")
 
-        # Write back to the JSON file
-        with open(input_file, 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
+            # Write back to the JSON file
+            with open(input_file, 'w', encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
 
-        print("Translation completed offline and updated in the JSON file.")
+            print("Translation completed offline and updated in the JSON file.")
+        except Exception as ex:
+            print(ex)
 
     def generate_translated_file(self):
-        # Assume JSON file has been updated with translations
-        # self.file_name = "test.docx"
-        output_file_path = os.path.join(self.output_folder, self.file_name)
-        json_file = f"{self.temp_folder}/{self.temp_file}.{self.temp_file_extension}"
-        with open(json_file, 'r', encoding='utf-8') as f:
-            translations = json.load(f)
+        try:
+            # Assume JSON file has been updated with translations
+            # self.file_name = "test.docx"
+            output_file_path = os.path.join(self.output_folder, self.file_name)
+            json_file = f"{self.temp_folder}/{self.temp_file}.{self.temp_file_extension}"
+            with open(json_file, 'r', encoding='utf-8') as f:
+                translations = json.load(f)
 
-        document_xml_path = os.path.join(self.temp_folder, "word", "document.xml")
-        if os.path.exists(document_xml_path):
-            # Parse the document.xml file to extract text
-            tree = ET.parse(document_xml_path)
-            root = tree.getroot()
-            namespace = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+            document_xml_path = os.path.join(self.temp_folder, "word", "document.xml")
+            if os.path.exists(document_xml_path):
+                # Parse the document.xml file to extract text
+                tree = ET.parse(document_xml_path)
+                root = tree.getroot()
+                namespace = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
 
-        # Step 4: Replace original text in document.xml with translations
-        if os.path.exists(document_xml_path):
-            for text_node in root.findall(".//w:t", namespace):  # type: ignore
-                original_text = text_node.text
-                if original_text in [list(item.keys())[0] for item in translations]:
-                    # Find the corresponding translation value
-                    translation_value = [item[original_text] for item in translations if original_text in item][0]
-                    text_node.text = translation_value
+            # Step 4: Replace original text in document.xml with translations
+            if os.path.exists(document_xml_path):
+                for text_node in root.findall(".//w:t", namespace):  # type: ignore
+                    original_text = text_node.text
+                    if original_text in [list(item.keys())[0] for item in translations]:
+                        # Find the corresponding translation value
+                        translation_value = [item[original_text] for item in translations if original_text in item][0]
+                        text_node.text = translation_value
 
-            # Write back the updated XML
-            tree.write(document_xml_path, encoding='utf-8', xml_declaration=True)  # type: ignore
+                # Write back the updated XML
+                tree.write(document_xml_path, encoding='utf-8', xml_declaration=True)  # type: ignore
 
-        # Step 5: Recreate the .docx file from extracted content
-        temp_zip = shutil.make_archive(os.path.join(self.output_folder, "temp_docx"), "zip", self.temp_folder)
-        os.rename(temp_zip, os.path.join(self.output_folder, f"{self.translated_file_prefix}_From_{Helper.get_language_name(self.source_lang)}_To_{Helper.get_language_name(self.target_lang)}_{self.file_name}"))
+            # Step 5: Recreate the .docx file from extracted content
+            temp_zip = shutil.make_archive(os.path.join(self.output_folder, "temp_docx"), "zip", self.temp_folder)
+            os.rename(temp_zip, os.path.join(self.output_folder, f"{self.translated_file_prefix}_From_{Helper.get_language_name(self.source_lang)}_To_{Helper.get_language_name(self.target_lang)}_{self.file_name}"))
 
-        # Clean up temporary files if desired
-        shutil.rmtree(self.temp_folder)
-        print(f"Recreated .docx file saved as: {output_file_path}")
+            # Clean up temporary files if desired
+            shutil.rmtree(self.temp_folder)
+            print(f"Recreated .docx file saved as: {output_file_path}")
+        except Exception as ex:
+            print(ex)
 
     def process_folder(self):
-        for self.file_name in os.listdir(self.input_folder):
-            self.extract_files_for_translating()
-            self.translate_extracted_file()
-            self.generate_translated_file()
+        try:
+            for self.file_name in os.listdir(self.input_folder):
+                self.extract_files_for_translating()
+                self.translate_extracted_file()
+                self.generate_translated_file()
+        except Exception as ex:
+            print(ex)
 
 
 if __name__ == "__main__":
