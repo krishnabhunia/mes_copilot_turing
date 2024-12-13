31af842a52cf3d849074e691bff955e7a8f9e3cf
Krishna Bhunia <krishna-d.bhunia@capgemini.com>
2024-12-13 14:13:56 +0530

    test

--- Translator/pdf_to_doc.py
+++ Translator/pdf_to_doc.py
@@ -1,9 +1,13 @@
 from pdf2docx import Converter
 
-# Convert PDF to DOCX
-pdf_file = "example.pdf"
-docx_file = "output.docx"
 
-converter = Converter(pdf_file)
-converter.convert(docx_file)
-converter.close()
+try:
+    # Convert PDF to DOCX
+    pdf_file = "input_pdf/English_PDF.pdf"
+    docx_file = "output.docx"
+
+    converter = Converter(pdf_file)
+    converter.convert(docx_file)
+    converter.close()
+except Exception as ex:
+    print(ex)
